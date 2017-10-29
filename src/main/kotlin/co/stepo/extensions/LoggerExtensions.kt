6dcfef1a2eaeaccd9cff35c9bd3957d83e685778
hiro <stepout.main@gmail.com>
2017-10-29 22:48:35 +0900

    polish logs extension

--- src/main/kotlin/co/stepo/extensions/LoggerExtensions.kt
+++ src/main/kotlin/co/stepo/extensions/LoggerExtensions.kt
@@ -4,122 +4,61 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.companionObjectInstance
 
-inline fun Logger.error(message: () -> String) {
-    if (this.isErrorEnabled) {
-        this.error(message())
-    }
-}
-
-inline fun Logger.error(t: Throwable, message: () -> String) {
+inline fun Logger.error(t: Throwable? = null, message: () -> String) {
     if (this.isErrorEnabled) {
         this.error(message(), t)
     }
 }
 
-inline fun Logger.errorForList(messages: () -> List<String>) {
-    if (this.isErrorEnabled) {
-        messages().forEach { this.error(it) }
-    }
-}
-
-inline fun Logger.errorForList(t: Throwable, messages: () -> List<String>) {
+inline fun Logger.errorForList(t: Throwable? = null, messages: () -> List<String>) {
     if (this.isErrorEnabled) {
         messages().forEach { this.error(it, t) }
     }
 }
 
-inline fun Logger.warn(message: () -> String) {
-    if (this.isWarnEnabled) {
-        this.warn(message())
-    }
-}
-
-inline fun Logger.warn(t: Throwable, message: () -> String) {
+inline fun Logger.warn(t: Throwable? = null, message: () -> String) {
     if (this.isWarnEnabled) {
         this.warn(message(), t)
     }
 }
 
-inline fun Logger.warnForList(messages: () -> List<String>) {
-    if (this.isWarnEnabled) {
-        messages().forEach { this.warn(it) }
-    }
-}
-
-inline fun Logger.warnForList(t: Throwable, messages: () -> List<String>) {
+inline fun Logger.warnForList(t: Throwable? = null, messages: () -> List<String>) {
     if (this.isWarnEnabled) {
         messages().forEach { this.warn(it, t) }
     }
 }
 
-inline fun Logger.info(message: () -> String) {
-    if (this.isInfoEnabled) {
-        this.info(message())
-    }
-}
-
-inline fun Logger.info(t: Throwable, message: () -> String) {
+inline fun Logger.info(t: Throwable? = null, message: () -> String) {
     if (this.isInfoEnabled) {
         this.info(message(), t)
     }
 }
 
-inline fun Logger.infoForList(messages: () -> List<String>) {
-    if (this.isInfoEnabled) {
-        messages().forEach { this.info(it) }
-    }
-}
-
-inline fun Logger.infoForList(t: Throwable, messages: () -> List<String>) {
+inline fun Logger.infoForList(t: Throwable? = null, messages: () -> List<String>) {
     if (this.isInfoEnabled) {
         messages().forEach { this.info(it, t) }
     }
 }
 
-inline fun Logger.debug(message: () -> String) {
-    if (this.isDebugEnabled) {
-        this.debug(message())
-    }
-}
-
-inline fun Logger.debug(t: Throwable, message: () -> String) {
+inline fun Logger.debug(t: Throwable? = null, message: () -> String) {
     if (this.isDebugEnabled) {
         this.debug(message(), t)
     }
 }
 
-inline fun Logger.debugForList(messages: () -> List<String>) {
-    if (this.isDebugEnabled) {
-        messages().forEach { this.debug(it) }
-    }
-}
-
-inline fun Logger.debugForList(t: Throwable, messages: () -> List<String>) {
+inline fun Logger.debugForList(t: Throwable? = null, messages: () -> List<String>) {
     if (this.isDebugEnabled) {
         messages().forEach { this.debug(it, t) }
     }
 }
 
-inline fun Logger.trace(message: () -> String) {
-    if (this.isTraceEnabled) {
-        this.trace(message())
-    }
-}
-
-inline fun Logger.trace(t: Throwable, message: () -> String) {
+inline fun Logger.trace(t: Throwable? = null, message: () -> String) {
     if (this.isTraceEnabled) {
         this.trace(message(), t)
     }
 }
 
-inline fun Logger.traceForList(messages: () -> List<String>) {
-    if (this.isTraceEnabled) {
-        messages().forEach { this.trace(it) }
-    }
-}
-
-
-inline fun Logger.traceForList(t: Throwable, messages: () -> List<String>) {
+inline fun Logger.traceForList(t: Throwable? = null, messages: () -> List<String>) {
     if (this.isTraceEnabled) {
         messages().forEach { this.trace(it, t) }
     }
